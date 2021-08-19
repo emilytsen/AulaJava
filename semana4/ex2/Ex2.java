f6c8bdc7031f4451d5d618e3d237dca59ea046e5
Emily Tsen <77694772+emilytsen@users.noreply.github.com>
2021-08-19 16:41:00 -0300

    ex2

---
+++ semana4/ex2/Ex2.java
@@ -0,0 +1,12 @@
+public class Ex2 {
+  
+    public static void main(String[] args) {
+
+        //Imprima a soma de 1 até 1000;
+        for (int n = 1; n <= 1000; n++) {
+            int soma = n++;
+            System.out.println( n + " + 1 = " + soma);
+        }
+        }
+  
+}
